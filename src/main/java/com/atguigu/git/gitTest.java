61dc4e506d99aaf39b6e1b957fd5504a1a1ce348
zj <zj@atguigu.com>
2023-04-03 16:53:39 +0800

    second commit

--- src/main/java/com/atguigu/git/gitTest.java
+++ src/main/java/com/atguigu/git/gitTest.java
@@ -3,5 +3,6 @@ package com.atguigu.git;
 public class gitTest {
     public static void main(String[] args) {
         System.out.println("hello world");
+        System.out.println("second commit");
     }
 }
